3e56001a0d112ff817e1b46d1dda23a4673de9ec
Ikgyu Shin Tom <58256111+ikgyu98@users.noreply.github.com>
2023-08-26 18:47:19 -0400

    Update FR_Set_2_maker.R

--- vaccine-inequality/FR_Set_2_maker.R
+++ vaccine-inequality/FR_Set_2_maker.R
@@ -1,44 +1,3 @@
-# library(tidyverse)
-# library(readxl)
-# library(readr)
-# library("writexl")
-# 
-# 
-# 
-# France_SES <- read_csv("Desktop/research/France Raw Dataset/region SES.csv")[c(1,2,3,4,15)] %>%
-#   rename(location = "REG_ID") %>%
-#   add_column(country = "France", .before = "location") %>%
-#   rename(value = "Value")%>%
-#   mutate(measure = case_when(
-#     endsWith(Indicator, "Unemployment rate") ~ "labor2",
-#     endsWith(Indicator, "Standardised mortality rate") ~ "wellbeing2",
-#     endsWith(Indicator, "Share of labour force with at least secondary education") ~ "edu1",
-#     endsWith(Indicator, "Employment rate") ~ "labor1",
-#     endsWith(Indicator, "Life expectancy at birth") ~ "wellbeing1",
-#     endsWith(Indicator, "Air pollution, level of PM2.5") ~ "environ1",
-#     endsWith(Indicator, "Number of rooms per person") ~ "housing1",
-#     endsWith(Indicator, "Share of households with internet broadband access") ~ "housing2",
-#     endsWith(Indicator, "Disposable income per capita") ~ "income2",
-#     endsWith(Indicator, "Perceived social network support") ~ "etc1",
-#     endsWith(Indicator, "Homicide rate") ~ "etc2",
-#     endsWith(Indicator, "Self-evaluation of life satisfaction") ~ "etc3",
-#     endsWith(Indicator, "Perception of corrution") ~ "etc4",
-#     endsWith(Indicator, "Voter turnout in general election") ~ "etc5",
-#     TRUE ~ as.character(Indicator))
-#   )%>%
-#   mutate(IND = NULL) %>%
-#   mutate(Indicator = NULL)%>%
-#   mutate(location = NULL)%>%
-#   rename(location = "Regions")%>%
-#   relocate(measure, .before = value)
-# 
-# View(France_SES)
-# 
-# 
-# #colnames()
-# #View(SES)
-# 
-
 library(tidyverse)
 library(readxl)
 library(readr)
@@ -106,14 +65,10 @@ a<-France_SES %>%
 
 
 
-View(a)
-
-
 
 
 
 #write_xlsx(a,"/Users/mac/Desktop/research/FR_Set_2.xlsx")
 
 
-table(a$location)
 
