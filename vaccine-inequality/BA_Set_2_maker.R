25894dba9af0a7b8025e01367b84ff18c77db2d5
Ikgyu Shin Tom <58256111+ikgyu98@users.noreply.github.com>
2023-08-26 18:46:05 -0400

    Update BA_Set_2_maker.R

--- vaccine-inequality/BA_Set_2_maker.R
+++ vaccine-inequality/BA_Set_2_maker.R
@@ -8,7 +8,7 @@ library(readr)
 library(convertr)
 
 
-ddd<- read_csv("/Users/mac/Desktop/research/bangladesh/dataset/income_by_union.csv")%>%
+temp<- read_csv("/Users/mac/Desktop/research/bangladesh/dataset/income_by_union.csv")%>%
   drop_na()%>%
   select(District, income)%>%
   group_by(District)%>%
@@ -18,11 +18,5 @@ ddd<- read_csv("/Users/mac/Desktop/research/bangladesh/dataset/income_by_union.c
   add_column(measure = "income7", .after="location")
   
   
-write_csv(ddd,"/Users/mac/Desktop/research/bangladesh/dataset/BA_Set_2.csv")
+write_csv(temp,"/Users/mac/Desktop/research/bangladesh/dataset/BA_Set_2.csv")
 
-ddd
-
-table(income$Division)
-
-colnames(income)
-table(income$District)
